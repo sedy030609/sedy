b162c9d3d30218a12f3f2a060cf637b6b904df5a
sedy030609 <142620809+sedy030609@users.noreply.github.com>
2025-07-15 14:39:18 +0200

    Předelan client.lua
    jaj- by sedy030609

---
+++ client.lua
@@ -0,0 +1,20 @@
+SConfig = {}
+
+SConfig.DiscordColors = {
+    Green = 56108,
+    Grey = 8421504,
+    Red = 16711680,
+    Orange = 16744192,
+    Blue = 2061822,
+    Purple = 11750815
+}
+
+SConfig.DiscordReportsLogWebhook = 'WEBHOOK'
+
+SConfig.DiscordPlayersReportScreenshotsWebhook = 'WEBHOOK'
+SConfig.DiscordReportsTranscriptWebhook = 'WEBHOOK'
+
+-- check https://documentation.rcore.cz/paid-resources/rcore_report/config#quickreportscustomwebhooks
+SConfig.QuickReportsCustomWebhooks = {
+    -- ['Found a bug'] = 'WEBHOOK',
+}
